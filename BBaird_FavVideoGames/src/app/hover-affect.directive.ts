e7dff67c658667fb0806e0a6aba04e0b7570c497
Brooke Baird <w0766373@myscc.ca>
2022-02-16 22:08:23 -0500

    Create Hover directive for hover event - Tags are bolded and underlined on hover - Return to none when not hovered over

--- BBaird_FavVideoGames/src/app/hover-affect.directive.ts
+++ BBaird_FavVideoGames/src/app/hover-affect.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
+  constructor(private elm: ElementRef) { }
 
-  constructor() { }
+  @HostListener("mouseenter") onMouseEnter(){
+    this.underline(true);
+  }
+  @HostListener("mouseleave") onMouseLeave(){
+    this.underline(false);
+  }
+  private underline(isHovered: boolean){
+    
+    this.elm.nativeElement.style.textDecoration = isHovered ? "underline" : "none";
+    this.elm.nativeElement.style.fontWeight = isHovered ? "bold" : "normal";
+    this.elm.nativeElement.style.border = isHovered ? "2px solid blue" : "none";
 
+
+    
+  }
 }
+
